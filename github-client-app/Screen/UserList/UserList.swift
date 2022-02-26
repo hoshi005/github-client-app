381773223ceef3049328f5fb0bcb0c95516d61e4
hoshi005 <hoshi005@me.com>
2022-02-26 16:12:33 +0900

    feat: ユーザ一覧画面の基本実装

--- github-client-app/Screen/UserList/UserList.swift
+++ github-client-app/Screen/UserList/UserList.swift
@@ -6,15 +6,51 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct UserList: View {
+    
+    @StateObject private var viewModel = UserListViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Image(systemName: "magnifyingglass")
+                    .font(.title2)
+                    .foregroundColor(.secondary)
+                TextField("githubユーザ名を入力してください", text: $viewModel.searchText)
+                    .disableAutocorrection(true)
+            }
+            .padding(.vertical, 12)
+            .padding(.horizontal)
+            .background(
+                Capsule()
+                    .strokeBorder(Color.accentColor, lineWidth: 1.0)
+            )
+            .padding(.horizontal)
+            
+            List {
+                ForEach(viewModel.users) { user in
+                    HStack {
+                        WebImage(url: user.avatarUrl)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 60, height: 60)
+                            .clipShape(Circle())
+                        Text(user.login)
+                            .font(.title2)
+                            .fontWeight(.bold)
+                    }
+                }
+            }
+        }
     }
 }
 
 struct UserList_Previews: PreviewProvider {
     static var previews: some View {
-        UserList()
+        NavigationView {
+            UserList()
+        }
     }
 }
