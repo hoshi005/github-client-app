17585db2fe4e153f2a2a5717e9b8e514daaf84b6
hoshi005 <hoshi005@me.com>
2022-02-26 10:14:53 +0900

    first commit

---
+++ github-client-app/github_client_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_client_appApp.swift
+//  github-client-app
+//
+//  Created by Susumu Hoshikawa on 2022/02/26.
+//
+
+import SwiftUI
+
+@main
+struct github_client_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
